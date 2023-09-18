5d299afd3724256eb83612c902e656019d4b675b
MD ARAFAT KOYES <125794297+arafdewann@users.noreply.github.com>
2023-09-18 16:27:59 -0400

    Workshop 2

---
+++ w2p1.c
@@ -0,0 +1,142 @@
+/*          Workshop -  02 (Part-01)*/
+/*
+FULL NAME : MD ARAFAT KOYES
+Student#ID: 133682229
+Email     : makoyes@myseneca.ca
+Section   : ZRA
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*/
+#include <stdio.h>
+
+int main(void) {
+    float smallShirtPrice, mediumShirtPrice, largeShirtPrice;
+    int numPattyShirts, numTommyShirts, numSallyShirts;
+
+    // Set shirt prices
+    printf("Set Shirt Prices\n");
+    printf("================\n");
+    printf("Enter the price for a SMALL shirt: $");
+    scanf("%f", &smallShirtPrice);
+    printf("Enter the price for a MEDIUM shirt: $");
+    scanf("%f", &mediumShirtPrice);
+    printf("Enter the price for a LARGE shirt: $");
+    scanf("%f", &largeShirtPrice);
+
+    // Display shirt price list
+    printf("\nShirt Store Price List\n");
+    printf("======================\n");
+    printf("SMALL  : $%.2f\n", smallShirtPrice);
+    printf("MEDIUM : $%.2f\n", mediumShirtPrice);
+    printf("LARGE  : $%.2f\n", largeShirtPrice);
+
+    // Get Patty's order details
+    printf("\nPatty's shirt size is 'S'\n");
+    printf("Number of shirts Patty is buying: ");
+    scanf("%d", &numPattyShirts);
+
+    // Get Tommy's order details
+    printf("\nTommy's shirt size is 'L'\n");
+    printf("Number of shirts Patty is buying: ");
+    scanf("%d", &numTommyShirts);
+
+    // Get Sally's order details
+    printf("\nPatty's shirt size is 'M'\n");
+    printf("Number of shirts Patty is buying: ");
+    scanf("%d", &numSallyShirts);
+
+    // Calculate the subtotal for each customer
+    float patty_subtotal = smallShirtPrice * numPattyShirts;
+    float tommy_subtotal = largeShirtPrice * numTommyShirts;
+    float sally_subtotal = mediumShirtPrice * numSallyShirts;
+
+    // Calculate the tax for each customer
+    float tax_rate = 0.13;
+    float patty_tax = patty_subtotal * tax_rate;
+    float tommy_tax = tommy_subtotal * tax_rate;
+    float sally_tax = sally_subtotal * tax_rate;
+
+    // Calculate the total sales and total tax
+    float total_sales = patty_subtotal + tommy_subtotal + sally_subtotal;
+    float total_tax = patty_tax + tommy_tax + sally_tax;
+    float total_amount = total_sales + total_tax;
+
+    // Display the sales information
+    printf("Customer\tSize\tPrice\tQty\tSub-Total\tTax\tTotal\n");
+    printf("--------\t----\t-----\t---\t---------\t-----\t-----\n");
+    printf("Patty\t\t%c\t%.2f\t%d\t%.4f\t%.4f\t%.4f\n", patty_size, small_price, patty_qty, patty_subtotal, patty_tax, patty_subtotal + patty_tax);
+    printf("Sally\t\t%c\t%.2f\t%d\t%.4f\t%.4f\t%.4f\n", sally_size, medium_price, sally_qty, sally_subtotal, sally_tax, sally_subtotal + sally_tax);
+    printf("Tommy\t\t%c\t%.2f\t%d\t%.4f\t%.4f\t%.4f\n", tommy_size, large_price, tommy_qty, tommy_subtotal, tommy_tax, tommy_subtotal + tommy_tax);
+    printf("--------\t----\t-----\t---\t---------\t-----\t-----\n");
+    printf("\t\t\t\t\t%.4f\t%.4f\t%.4\n");
+
+    printf("Daily retail sales represented by coins\n");
+    printf("=======================================\n");
+
+
+   // Total sales amount
+     double total_sales = 365.57; 
+
+
+   // Define the values of each coin denomination
+        double toonie = 2.00;
+        double loonie = 1.00;
+        double quarter = 0.25;
+        double dime = 0.10;
+        double nickel = 0.05;
+        double penny = 0.01;
+
+   // Calculate the number of coins for each denomination
+        int num_toonies = total_sales / toonie;
+        total_sales -= num_toonies * toonie;
+
+        int num_loonies = total_sales / loonie;
+        total_sales -= num_loonies * loonie;
+
+        int num_quarters = total_sales / quarter;
+        total_sales -= num_quarters * quarter;
+
+        int num_dimes = total_sales / dime;
+        total_sales -= num_dimes * dime;
+
+        int num_nickels = total_sales / nickel;
+        total_sales -= num_nickels * nickel;
+
+        int num_pennies = total_sales / penny;
+        total_sales -= num_pennies * penny;
+
+    // Display the breakdown of sales by coin denominations
+        printf("Daily retail sales represented by coins\n");
+        printf("=======================================\n");
+        printf("Coin\t\tQty\n");
+        printf("--------\t---\n");
+        printf("Toonies\t\t%d\n", num_toonies);
+        printf("Loonies\t\t%d\n", num_loonies);
+        printf("Quarters\t%d\n", num_quarters);
+        printf("Dimes\t\t%d\n", num_dimes);
+        printf("Nickels\t\t%d\n", num_nickels);
+        printf("Pennies\t\t%d\n", num_pennies);
+        printf("\n");
+        float total_sales = patty_subtotal + tommy_subtotal + sally_subtotal;
+        float total_Quantity = numPattyShirts+numTommyShirts+numSallyShirts
+        printf("Average cost/shirt: total_sales / total_Quantity ")
+
+        printf("\n")
+        printf("Sales EXCLUDING tax\n");
+        printf("Coin\t\tQty\tBalance\n");
+        printf("--------\t---\t---------\n");
+
+        // Calculate and display the breakdown of sales excluding tax
+        double balance = total_sales;
+        int num_toonies = balance / 2;
+        balance -= num_toonies * 2;
+        printf("Toonies\t\t%d\t%.4lf\n", num_toonies, balance);
+        printf("Average cost/shirt: total_sales / total_Quantity ")
+
+
+
+   return 0;
+}
---
+++ w2p2.c
@@ -0,0 +1,159 @@
+/*/////////////////////////////////////////////////////////////////////////
+                          Workshop - #2 (P2)
+Full Name  : MD ARAFAT KOYES
+Student ID#: 13368229
+Email      : makoyes@myseneca.ca
+Section    : ZRA
+
+Authenticity Declaration:
+
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+    const double TAX = 0.13;  // Tax rate
+    const char patSize = 'S', salSize = 'M', tomSize = 'L';  // Sizes of shirts
+    double SMALL;  // Price of a small shirt
+    double MEDIUM;  // Price of a medium shirt
+    double LARGE;  // Price of a large shirt
+    int pnumber;  // Number of shirts Patty is buying
+    int tnumber;  // Number of shirts Tommy is buying
+    int snumber;  // Number of shirts Sally is buying
+    int subTotalp;  // Subtotal for Patty's shirts
+    int taxp;  // Tax amount for Patty's shirts
+    int totalp;  // Total amount for Patty's shirts
+    int subTotals, taxs, totals;  
+    int subTotalt, taxt, totalt;  
+    int fsubTotal, ftax, ftotal;  
+    int tooniesTotal, looniesTotal, quarterTotal, dimesTotal, nickelsTotal, penniesTotal;  // Coin totals for sales excluding tax
+    int remtoon, remloon, remquarter, remdime, remnickel, rempenni;  
+    int avrg, fnumber, avrgwtax;  // Average values and total number of shirts sold
+    int toonTotal, loonTotal, quartTotal, dimeTotal, nickTotal, penniTotal;  
+    int remtoonies, remloonies, remquarters, remdimes, remnickels, rempennis;  // Remaining amounts after coin calculations
+
+    // Set shirt prices
+    printf("Set Shirt Prices");
+    printf("\n================");
+    printf("\nEnter the price for a SMALL shirt: $");
+    scanf("%lf", &SMALL);
+    printf("Enter the price for a MEDIUM shirt: $");
+    scanf("%lf", &MEDIUM);
+    printf("Enter the price for a LARGE shirt: $");
+    scanf("%lf", &LARGE);
+
+    // Display shirt prices
+    printf("\nShirt Store Price List");
+    printf("\n======================");
+    printf("\nSMALL  : $%0.2lf", SMALL);
+    printf("\nMEDIUM : $%0.2lf", MEDIUM);
+    printf("\nLARGE  : $%0.2lf", LARGE);
+
+    // Get the number of shirts each customer is buying
+    printf("\n\nPatty's shirt size is '%c'", patSize);
+    printf("\nNumber of shirts Patty is buying: ");
+    scanf("%d", &pnumber);
+    printf("\nTommy's shirt size is '%c'", tomSize);
+    printf("\nNumber of shirts Tommy is buying: ");
+    scanf("%d", &tnumber);
+    printf("\nSally's shirt size is '%c'", salSize);
+    printf("\nNumber of shirts Sally is buying: ");
+    scanf("%d", &snumber);
+
+    // Calculate subtotals, taxes, and totals for each customer
+    subTotalp = (SMALL * 100) * pnumber;
+    taxp = (subTotalp * TAX) + 0.5;
+    totalp = subTotalp + taxp;
+    subTotals = (MEDIUM * 100) * snumber;
+    taxs = (subTotals * TAX) + 0.5;
+    totals = subTotals + taxs;
+    subTotalt = (LARGE * 100) * tnumber;
+    taxt = (subTotalt * TAX) + 0.5;
+    totalt = subTotalt + taxt;
+    fsubTotal = subTotalp + subTotals + subTotalt;
+    ftax = taxp + taxs + taxt;
+    ftotal = totalp + totals + totalt;
+
+    // Display the customer and order details
+    printf("\nCustomer Size Price Qty Sub-Total       Tax     Total");
+    printf("\n-------- ---- ----- --- --------- --------- ---------");
+    printf("\nPatty    %-4c %5.2lf %3d %9.4lf %9.4lf %9.4lf", patSize, SMALL, pnumber, (double)subTotalp / 100, (double)taxp / 100, (double)totalp / 100);
+    printf("\nSally    %-4c %5.2lf %3d %9.4lf %9.4lf %9.4lf", salSize, MEDIUM, snumber, (double)subTotals / 100, (double)taxs / 100, (double)totals / 100);
+    printf("\nTommy    %-4c %5.2lf %3d %9.4lf %9.4lf %9.4lf", tomSize, LARGE, tnumber, (double)subTotalt / 100, (double)taxt / 100, (double)totalt / 100);
+    printf("\n-------- ---- ----- --- --------- --------- ---------");
+    printf("\n%33.4lf %9.4lf %9.4lf", (double)fsubTotal / 100, (double)ftax / 100, (double)ftotal / 100);
+
+    // Calculate coin totals for sales excluding tax
+    tooniesTotal = fsubTotal / 2;
+    remtoon = fsubTotal % 200;
+    looniesTotal = remtoon / 1;
+    remloon = remtoon % 100;
+    quarterTotal = remloon / 0.25;
+    remquarter = remloon % 25;
+    dimesTotal = remquarter / 0.10;
+    remdime = remquarter % 10;
+    nickelsTotal = remdime / 0.50;
+    remnickel = remdime % 05;
+    penniesTotal = remnickel / 0.01;
+    rempenni = remnickel % 1;
+
+    // Calculate average cost per shirt and total number of shirts sold
+    fnumber = pnumber + tnumber + snumber;
+    avrg = (fsubTotal * 1000) / fnumber + 0.5;
+
+    // Display coin totals for sales excluding tax
+    printf("\n\nDaily retail sales represented by coins");
+    printf("\n=======================================");
+    printf("\n\nSales EXCLUDING tax");
+    printf("\nCoin     Qty   Balance");
+    printf("\n-------- --- ---------");
+    printf("\n%22.4lf", (double)fsubTotal / 100);
+    printf("\nToonies  %3d %9.4lf", tooniesTotal / 100, (double)remtoon / 100);
+    printf("\nLoonies  %3d %9.4lf", looniesTotal / 100, (double)remloon / 100);
+    printf("\nQuarters %3d %9.4lf", quarterTotal / 100, (double)remquarter / 100);
+    printf("\nDimes    %3d %9.4lf", dimesTotal / 100, (double)remdime / 100);
+    printf("\nNickels  %3d %9.4lf", nickelsTotal / 100, (double)remnickel / 100);
+    printf("\nPennies  %3d %9.4lf", penniesTotal / 100, (double)rempenni / 100);
+
+    // Display average cost per shirt
+    printf("\n\nAverage cost/shirt: $%2.4lf", (double)avrg / 100000);
+
+    // Calculate coin totals for sales including tax
+    toonTotal = ftotal / 2;
+    remtoonies = ftotal % 200;
+    loonTotal = remtoonies / 1;
+    remloonies = remtoonies % 100;
+    quartTotal = remloonies / 0.25;
+    remquarters = remloonies % 25;
+    dimeTotal = remquarters / 0.10;
+    remdimes = remquarters % 10;
+    nickTotal = remdimes / 0.05;
+    remnickels = remdimes % 05;
+    penniTotal = remnickels / 0.01;
+    rempennis = remnickels % 1;
+    avrgwtax = (ftotal * 1000) / fnumber + 0.5;
+
+    // Display coin totals for sales including tax
+    printf("\n\nSales INCLUDING tax");
+    printf("\nCoin     Qty   Balance");
+    printf("\n-------- --- ---------");
+    printf("\n%22.4lf", (double)ftotal / 100);
+    printf("\nToonies  %3d %9.4lf", toonTotal / 100, (double)remtoonies / 100);
+    printf("\nLoonies  %3d %9.4lf", loonTotal / 100, (double)remloonies / 100);
+    printf("\nQuarters %3d %9.4lf", quartTotal / 100, (double)remquarters / 100);
+    printf("\nDimes    %3d %9.4lf", dimeTotal / 100, (double)remdimes / 100);
+    printf("\nNickels  %3d %9.4lf", nickTotal / 100, (double)remnickels / 100);
+    printf("\nPennies  %3d %9.4lf", penniTotal / 100, (double)rempennis / 100);
+
+    // Display average cost per shirt including tax
+    printf("\n\nAverage cost/shirt: $%2.4lf", (double)avrgwtax / 100000);
+    printf("\n");
+
+    return 0;
+}
